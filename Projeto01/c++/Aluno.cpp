410e94ace847daf8cef29614fd91eb0488509052
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-07 16:20:10 -0300

    Media aprovacao

--- Projeto01/c++/Aluno.cpp
+++ Projeto01/c++/Aluno.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h> 
 
-//#define MAX_PROVA 10;
+#define MAX_PROVAS 10 // Limites max de provas que oo sistema 
 
-int qtdAlunos = 0, qtdProvas; // Var acumulativa 
+int qtdAlunos = 0, qtdProvas, mediaAprovacao = 0; // Var acumulativa 
 
 
 // Definição da estrutura Aluno, que agrupa os dados de cada aluno
 struct Aluno {
     char Nome[30];  // Esse campo so permite 30 caractere    
-    float Notas[3];   
+    float Notas[MAX_PROVAS];   
    // char Sexo;         
-   // bool Resultado;      
+    bool Resultado;      
 };
 
 int main() {
@@ -19,31 +19,34 @@ int main() {
     Aluno Turma[100]; // Vector de limite max de 100 alunos por turma
     char Resposta;
 
+    // Regra de negocio
+    // Quantas provas vao ser aplicado para todos os alunos
+    do {
+        printf("\n   Infrome a quantidade de provas: ");
+        scanf("%i", &qtdProvas);
+
+        // Quantidade provas a ser aplicado do MAX 10 provas
+        if(qtdProvas < 1 || qtdProvas > MAX_PROVAS){
+            printf("\n Atencao! Entre 1 e 10 Provas.");
+        }
+
+        // Quantidade tem ser maior ou igual a um ou menor ou igual a dez
+    }while(qtdProvas < 1 || qtdProvas > MAX_PROVAS);// Operador OU e ||
+
+
+    // Media de aprovacao
+    printf("\n Informe a media de para aprovacao: ");
+    scanf("%f", &mediaAprovacao);
 
     do {
-
-
-        // Regra de negocio
-        do {
-            printf("\n   Infrome a quantidade de provas: ");
-            scanf("%i", &qtdProvas);
-
-            // Quantidade provas a ser aplicado do MAX 10 provas
-            if( qtdProvas < 1 || qtdProvas > 10){
-                printf("\n Atencao! Entre 1 e 10 Provas.");
-            }
-
-            // Quantidade tem ser maior ou igual a um ou menor ou igual a dez
-        }while(qtdProvas < 1 || qtdProvas > 10);
-
         
         // Leia Nome do aluno e guarda o nome na posicao qtdAlunos no vetor Turma
-        printf("    Informe o nome do %i. aluno: ", qtdAlunos + 1);
+        printf("\n   Informe o nome do %i. aluno: ", qtdAlunos + 1);
         scanf("%s", Turma[qtdAlunos].Nome); 
 
 
         // Pecorrendo o vetor na linha 09
-        for (int i = 1; i <= 2; i++){
+        for (int i = 1; i <= qtdProvas; i++){
 
             // Leia uma nota e guarde na posicao I das notas do aluno de numero qtdAlunos
             printf("    Informe a %i. nota: ", i);
@@ -52,16 +55,17 @@ int main() {
             // A primeira posicao do vetor esta acomulando a soma das notas
             Turma[qtdAlunos].Notas[0] += Turma[qtdAlunos].Notas[i]; 
         }
-        //Turma[qtdAlunos].Resultado = Turma[qtdAlunos].Notas[0] / 3 >= 6; 
+       Turma[qtdAlunos].Resultado = Turma[qtdAlunos].Notas[0] / qtdProvas >= mediaAprovacao; 
 
         // Pergunta se deseja adicionar mais alunos
         printf("Deseja cadastrar outro aluno? (S/N): ");
         scanf(" %c", &Resposta); // Corrigido: espaço antes do %c ignora enter anterior
 
-        //qtdAlunos++; // Incrementa a quantidade de alunos cadastrados
+
+
+        // Incrementa a quantidade de alunos cadastrados
+        qtdAlunos++; 
+
+
     } while (Resposta == 'S' || Resposta == 's');
-
-
-    printf("\n A soma das notas: %.2f", Turma[qtdAlunos].Notas[0]);
-    printf("\n A soma das notas: %.2f", Turma[qtdAlunos].Notas[0] / 3);
 }
