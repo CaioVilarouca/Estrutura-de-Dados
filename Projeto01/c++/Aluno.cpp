70fabe4635c18152b582b5f3f76fd49d1d7973fd
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-07 15:17:46 -0300

    Projeto em C++

--- Projeto01/c++/Aluno.cpp
+++ Projeto01/c++/Aluno.cpp
@@ -1,44 +1,57 @@
 #include <stdio.h>
 #include <stdbool.h> 
 
-int qtdAlunos = 0; // Var acumulativa 
+//#define MAX_PROVA 10;
+
+int qtdAlunos = 0, soma = 0, media; // Var acumulativa 
+
 
 // Definição da estrutura Aluno, que agrupa os dados de cada aluno
 struct Aluno {
     char Nome[30];  // Esse campo so permite 30 caractere    
     float Notas[3];   
- //  char Sexo;         
- //   bool Aprovado;      
+   // char Sexo;         
+   // bool Resultado;      
 };
 
 int main() {
+
     Aluno Turma[100]; // Vector de limite max de 100 alunos por turma
     char Resposta;
 
 
     do {
-        printf("    Informe o nome do %i. aluno: ", qtdAlunos + 1);
+
+
+        // Regra de negocio
+
+
+        
         // Leia Nome do aluno e guarda o nome na posicao qtdAlunos no vetor Turma
+        printf("    Informe o nome do %i. aluno: ", qtdAlunos + 1);
         scanf("%s", Turma[qtdAlunos].Nome); 
 
+
         // Pecorrendo o vetor na linha 09
-        for (int i = 1; i <= 3; i++){
+        for (int i = 1; i <= 2; i++){
 
             // Leia uma nota e guarde na posicao I das notas do aluno de numero qtdAlunos
             printf("    Informe a %i. nota: ", i);
-            scanf("%f", &Turma[qtdAlunos].Notas[i]);
+            scanf("%f", &Turma[qtdAlunos].Notas[i]);// Um agregador container
 
-            // Acumulando a nota do aluno
-            //Turma[qtdAlunos].Notas[0] += Turma[qtdAlunos].Notas[0];
+            // A primeira posicao do vetor esta acomulando a soma das notas
+            Turma[qtdAlunos].Notas[0] += Turma[qtdAlunos].Notas[i]; 
         }
+        //Turma[qtdAlunos].Resultado = Turma[qtdAlunos].Notas[0] / 3 >= 6; 
 
-        //Turma[qtdAlunos].Aprovado = Turma[qtdAlunos].Notas[0] / 3 >= 6; 
-          // Pergunta se deseja adicionar mais alunos
+        // Pergunta se deseja adicionar mais alunos
         printf("Deseja cadastrar outro aluno? (S/N): ");
         scanf(" %c", &Resposta); // Corrigido: espaço antes do %c ignora enter anterior
 
         //qtdAlunos++; // Incrementa a quantidade de alunos cadastrados
     } while (Resposta == 'S' || Resposta == 's');
 
-    
+
+    printf("\n A soma das notas: %.2f", Turma[qtdAlunos].Notas[0]);
+    printf("\n A soma das notas: %.2f", Turma[qtdAlunos].Notas[0] / 3);
 }
