9a2c348440e8892f7d773d3da901d61f8454bbee
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-13 22:29:45 -0300

    Add var NotaObtida + % de Aprov por sexo + Resultdo em tela em processo

--- Projeto01/c++/Aluno.cpp
+++ Projeto01/c++/Aluno.cpp
@@ -5,12 +5,13 @@
 #define MAX_PROVAS 10 // Limites max de provas 
 
 int qtdAlunos = 0, qtdProvas; // Var acumulativa 
-float mediaAprovacao;
+float MediaAprovacao;
 
 // Definicao da estrutura Aluno, que agrupa os dados de cada aluno
 struct Aluno { 
     char Nome[30];  // Esse campo so permite 30 caractere    
     float Notas[MAX_PROVAS];   
+    float NotaObtida; // media final do aluno
     char Sexo;         
     bool Resultado;      
 };
@@ -31,25 +32,25 @@ int main() {
         printf("\n Informe a quantidade de provas: (Limite max de 10 provas.)");
         scanf("%i", &qtdProvas);
 
-        // Quantidade provas a ser aplicado do MAX 10 provas
-        // || se a primeira condição já for true, não avalia a segunda
+        // Quantidade provas a ser aplicado MAX 10 provas
+        // || Se a primeira condicao ja for true, nao avalia a segunda
         if(qtdProvas < 1 || qtdProvas > MAX_PROVAS){
             printf("\n Atencao! Entre 1 e 10 Provas.");
         }
 
-        // || se a primeira condição já for true, não avalia a segunda
+        // || Se a primeira condicao ja for true, nao avalia a segunda
     }while(qtdProvas < 1 || qtdProvas > MAX_PROVAS);// Operador OU e ||
 
     do {
       // Media de aprovacao
       printf("\n Informe a media de para aprovacao: (Media entre 05 e 08): ");
-      scanf("%f", &mediaAprovacao);
-    } while ( mediaAprovacao < 5 || mediaAprovacao > 8);
+      scanf("%f", &MediaAprovacao);
+    } while ( MediaAprovacao < 5 || MediaAprovacao > 8);
 
     do {
         // Leia Nome do aluno e guarda o nome na posicao qtdAlunos no vetor Turma
         printf("\n Informe o nome do %i. aluno: ", qtdAlunos + 1);
-        scanf("%s", Turma[qtdAlunos].Nome); 
+        scanf(" %29[^\n]", Turma[qtdAlunos].Nome); 
 
         //  Entrada do sexo com validacao (aceita M ou F)
         do {
@@ -71,9 +72,10 @@ int main() {
         }
 
         // Calc a media do aluno
+        // Evita HARD CODED com a var MediaAprovacao
         // Resultado recebe o True ou False
-        Turma[qtdAlunos].Resultado=Turma[qtdAlunos].Notas[0] / qtdProvas >= mediaAprovacao; 
-        // Evita HARD CODED com a var mediaAprovacao
+        Turma[qtdAlunos].Resultado = Turma[qtdAlunos].Notas[0] / qtdProvas >= MediaAprovacao; // Resultado da media do aluno
+        Turma[qtdAlunos].NotaObtida = Turma[qtdAlunos].Notas[0] / qtdProvas; // Calc da nota final do aluno
 
         //  Atualiza as estatisticas de acordo com o sexo e se foi aprovado ou nao
         if (Turma[qtdAlunos].Sexo == 'M') {
@@ -96,23 +98,25 @@ int main() {
         qtdAlunos++;
     } while (Resposta == 'S' || Resposta == 's');
     // Fim do loop 
-    
-    // Resultado do dados coletados
+
+}
+// codigo antigo
+/*   // Resultado do dados coletados
     printf("\n  Quantidade de Alunos: [%d]  \n", qtdAlunos);
 
     printf("\n--- Alunos Aprovados ---\n");
     for (int i = 0; i < qtdAlunos; i++) {
         // true ou false
         if (Turma[i].Resultado) {
-            printf("Nome: %-10s | Sexo: %c | Situacao: [ Aprovado ] A media solicitada foi %0.2f \n", Turma[i].Nome, Turma[i].Sexo, mediaAprovacao);
+            printf("Nome: %-10s | Sexo: %c | Situacao: [Aprovado] A media solicitada foi %0.2f e a nota obtida \n", Turma[i].Nome, Turma[i].Sexo, MediaAprovacao);
         }
-    }
+    }        
 
     printf("\n--- Alunos Reprovados ---\n");
     for (int i = 0; i < qtdAlunos; i++) {
         // resultado for diferente de true 
         if (!Turma[i].Resultado) {
-            printf("Nome: %-10s | Sexo: %c | Situacao: [ Reprovado ] A media solicitada foi %0.2f \n", Turma[i].Nome, Turma[i].Sexo, mediaAprovacao);
+            printf("Nome: %-10s | Sexo: %c | Situacao: [Reprovado] A media solicitada foi %.2f e a nota obtida \n", Turma[i].Nome, Turma[i].Sexo, MediaAprovacao);
         }
     }
 
@@ -122,9 +126,11 @@ int main() {
     printf("  Total:     %d\n", totalMasc);
     printf("  Aprovados: %d\n", aprovMasc);
     printf("  Reprovados:%d\n", reprovMasc);
+    printf("  Porcetagem de alunos Masc %.2f \n", ((float)aprovMasc / totalMasc) * 100);
+  
+
 
     printf("\nFeminino:\n");
     printf("  Total:     %d\n", totalFem);
     printf("  Aprovadas: %d\n", aprovFem);
-    printf("  Reprovadas:%d\n", reprovFem);
-}
+    printf("  Reprovadas:%d\n", reprovFem);*/
