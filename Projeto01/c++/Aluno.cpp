ccb0bb75bbc86dc8175ce8d10b072b23af4ddb1e
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-20 23:31:20 -0300

    up

--- Projeto01/c++/Aluno.cpp
+++ Projeto01/c++/Aluno.cpp
@@ -6,7 +6,7 @@
 
 int qtdAlunos=0, qtdProvas, totalMasc=0, totalFem=0, aprovMasc=0, aprovFem=0, reprovMasc=0, reprovFem=0, Opcao=0; 
 float MediaAprovacao;
-char Resposta;
+char Resposta, Barra = 219;
 
 // Definicao da estrutura Aluno, que agrupa os dados de cada aluno
 struct Aluno { 
@@ -18,7 +18,7 @@ struct Aluno {
 };
 
 int main() {
-
+    
     Aluno Turma[100]; // Vetor de limite max de 100 alunos por turma
 
     // Regra de negocio
@@ -91,7 +91,7 @@ int main() {
         if (Turma[qtdAlunos].Sexo == 'M') {
             totalMasc++;
             if (Turma[qtdAlunos].Resultado)
-                aprovMasc++;
+                aprovMasc++;   
             else
                 reprovMasc++;
         } else {
@@ -113,14 +113,13 @@ int main() {
             scanf(" %c", &Resposta);
             Resposta = toupper(Resposta);
         }
-
         qtdAlunos++; 
     } while (Resposta == 'S');
     // Fim do loop 
 
    do {
         // Menu de registro aluno
-        printf ("\n[1] Registro de Aprovados\n[2] Registro de Reprovados\n[3] Estatistica\n[0] Sair\nInforme: ");
+        printf ("\n[1] Registro de Aprovados\n[2] Registro de Reprovados\n[3] Estatistica\n[4] Grafico\n[0] Sair\nInforme: ");
         scanf("%d", &Opcao);
 
         // Menu
@@ -150,16 +149,28 @@ int main() {
             printf("Porcetagem dda turma APROVADOS  %.2f \n", ((float)(aprovMasc + aprovFem) / qtdAlunos) * 100);
             printf("---------------------------------\n");
             printf("Porcetagem de alunos Masc REPROVADOS   %.2f \n", ((float)reprovMasc / totalMasc) * 100);
-            printf("Porcetagem de alunos Fem REPROVADOS    %.2f \n", ((float)reprovFem / totalFem) * 100);
+            printf("Porcetagem de alunos Fem REPROVADOS    %.2f \n", ((float)reprovMasc / totalFem) * 100);
             printf("Porcetagem da turma REPROVADOS  %.2f \n", ((float)(reprovMasc + reprovFem) / qtdAlunos) * 100);
+            break;
+        case 4:
+            printf("\nGrafico da turma de APROVADOS");
+            for (int i = 0; i < (aprovMasc + aprovFem); i++){
+                printf(" %c", Barra);
+            }
+            printf("\nGrafico da turma de Reprovados");
+            for (int i = 0; i < (reprovMasc + reprovMasc); i++){
+                printf(" %c", Barra);
+            }
             break;
         default:
             while (Opcao != 1 && Opcao != 2 && Opcao != 3 && Opcao != 0){
-                printf ("Esse campo so aceita esses valores!\n[1] Registro de Aprovados\n[3] Registro de Reprovados\n[3] Sair\nInforme: ");
+                printf ("Esse campo so aceita esses valores!\n[1] Registro de Aprovados\n[3] Registro de Reprovados\n[4] Grafico\n[0] Sair\nInforme: ");
                 scanf("%d", &Opcao);
             }
             break;
         }    
     } while (Opcao != 0);
+
+    
     printf("\n Encerramento de registro");
 }
